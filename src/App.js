55a5fdae63a5f55db73f0856655446de5041d2a4
Kristian Freeman <kristian@kristianfreeman.com>
2019-11-29 11:13:24 -0600

    Commit changes from video

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import ApolloClient from 'apollo-boost'
+import { gql } from 'apollo-boost'
+import { ApolloProvider, useQuery, useMutation } from '@apollo/react-hooks'
+
+const graphQLServerUrl = 'https://my-journal-server.signalnerve.workers.dev/'
+
+const client = new ApolloClient({
+  uri: graphQLServerUrl,
+})
+
+const GET_ENTRIES = gql`
+  {
+    entries {
+      id
+      text
+    }
+  }
+`
+
+const Entries = () => {
+  const { loading, error, data } = useQuery(GET_ENTRIES)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  return (
+    <div>
+      {data.entries.map(({ text }) => (
+        <h3>{text}</h3>
+      ))}
+    </div>
+  )
+}
+
+function AddEntry() {
+  let input
+  const [createEntry] = useMutation(
+    gql`
+      mutation CreateEntry($text: String!) {
+        createEntry(text: $text) {
+          id
+          text
+        }
+      }
+    `,
+    {
+      update(
+        cache,
+        {
+          data: { createEntry },
+        },
+      ) {
+        const { entries } = cache.readQuery({ query: GET_ENTRIES })
+        cache.writeQuery({
+          query: GET_ENTRIES,
+          data: { entries: [createEntry].concat(entries) },
+        })
+      },
+    },
+  )
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          createEntry({ variables: { text: input.value } })
+          input.value = ''
+        }}
+      >
+        <input
+          ref={node => {
+            input = node
+          }}
+        />
+        <button type="submit">Add Entry</button>
+      </form>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ApolloProvider client={client}>
+      <Entries />
+      <AddEntry />
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
